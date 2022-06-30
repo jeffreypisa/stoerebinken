5e079640f72640e01a6c8809e8f0a4186a40ff1d
Jeffrey Pisa <jeffrey.pisa@gmail.com>
2022-06-30 09:02:21 +0000

    misc

--- lib/svg.php
+++ lib/svg.php
@@ -4,4 +4,7 @@
     return $mimes;
   }
   add_filter('upload_mimes', 'cc_mime_types');
+  
+  add_filter('jpeg_quality', function($arg){return 90;});
+   add_filter('wp_editor_set_quality', function($arg){return 90;});
 ?>
